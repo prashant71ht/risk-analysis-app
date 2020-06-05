1cefa515a9d3ae23adcd0d2cf4702f1484279ad0
prashant <prashantt@heaptrace.com>
2020-06-05 17:39:36 +0530

    added dist code

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ var RISK_COLLECTION = "risk";
 
 var app = express();
 app.use(bodyParser.json());
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
